f03aba682bf738ce01c9b9d4a0946d5aaa37f68f
Donald Nguyen <donaldnguyen99@gmail.com>
2022-02-17 03:39:56 -0800

    formatting and debugging the install script, along with making the project not use the pre-build event by default

--- Blackjack/tools/install_sdl_visual_c.bat
+++ Blackjack/tools/install_sdl_visual_c.bat
@@ -8,12 +8,11 @@ rem command line:
 rem     call "$(ProjectDir)tools\install_sdl_visual_c.bat"
 rem
 rem This batch script assumes by default that
-rem 1. it is located your project's rem "tools" (or as another 
-rem    name) directory
-rem 2. your project directory is located under the solution's directory. 
+rem 1. it is located your project's "tools" (or as another name) directory, 
+rem 2. your project directory is located under the solution's directory, 
 rem        ex: SolDir\MyProj\tools\install_sdl_visual_c.bat
 rem 3. SDL directory (as a git submodule) is in the "Dependencies" 
-rem    directory which is under the solution's directory (and 
+rem    directory, which itself is under the solution's directory (and 
 rem    sibling to your project(s))
 rem        ex: SolDir\Dependencies\SDL
 rem
@@ -30,9 +29,18 @@ set sdl_version_tag=release-2.0.20
 rem ##############################################################
 
 
-rem Detecting Platform
+rem Detecting Environment and Platform
+if not exist devenv.exe (
+    echo This batch script must be run from Visual Studio's 
+    echo "Developer Command Prompt for VS XXXX" or as a Visual Studio
+    echo Pre-Built Event within the project's properties with the 
+    echo command line:
+    echo.    call "$(ProjectDir)tools\install_sdl_visual_c.bat"
+    exit 7
+) else (
+    echo devenv.exe detected. Continuing...
+)
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
-
 
 set script_dir=%~dp0
 pushd %~dp0..\..
@@ -50,9 +58,9 @@ if defined _GIT_FOUND (
 ) else (
     echo git.exe not found in PATH!
 )
-if exist .git\config (
+if exist "%sdl_dir%\include\" (
     set _git_submod=T
-    echo SDL git submodule found!
+    echo SDL git submodule installed!
 ) else (
     echo No SDL git submodule found!
 )
@@ -72,19 +80,19 @@ echo Looking for necessary SDL libraries from %install_dir%\SDL.sln
 
 echo Checking %install_dir%\Win32\Debug
 set d32=F
-if not exist %install_dir%\Win32\Debug\SDL2.dll (
+if not exist "%install_dir%\Win32\Debug\SDL2.dll" (
     set d32=T
     echo SDL2.dll not found, attempting to build
 )
-if not exist %install_dir%\Win32\Debug\SDL2.lib (
+if not exist "%install_dir%\Win32\Debug\SDL2.lib" (
     set d32=T
     echo SDL2.lib not found, attempting to build
 )
-if not exist %install_dir%\Win32\Debug\SDL2main.lib (
+if not exist "%install_dir%\Win32\Debug\SDL2main.lib" (
     set d32=T
     echo SDL2main.lib not found, attempting to build
 )
-if not exist %install_dir%\Win32\Debug\SDL2test.lib (
+if not exist "%install_dir%\Win32\Debug\SDL2test.lib" (
     set d32=T
     echo SDL2test.lib not found, attempting to build
 )
@@ -96,19 +104,19 @@ if "%d32%"=="T" (
 
 echo Checking %install_dir%\Win32\Release
 set r32=F
-if not exist %install_dir%\Win32\Release\SDL2.dll (
+if not exist "%install_dir%\Win32\Release\SDL2.dll" (
     set r32=T
     echo SDL2.dll not found, attempting to build
 )
-if not exist %install_dir%\Win32\Release\SDL2.lib (
+if not exist "%install_dir%\Win32\Release\SDL2.lib" (
     set r32=T
     echo SDL2.lib not found, attempting to build
 )
-if not exist %install_dir%\Win32\Release\SDL2main.lib (
+if not exist "%install_dir%\Win32\Release\SDL2main.lib" (
     set r32=T
     echo SDL2main.lib not found, attempting to build
 )
-if not exist %install_dir%\Win32\Release\SDL2test.lib (
+if not exist "%install_dir%\Win32\Release\SDL2test.lib" (
     set r32=T
     echo SDL2test.lib not found, attempting to build
 )
@@ -120,19 +128,19 @@ if "%r32%"=="T" (
 
 echo Checking %install_dir%\x64\Debug\
 set d64=F
-if "%OS%"=="64BIT" if not exist %install_dir%\x64\Debug\SDL2.dll (
+if "%OS%"=="64BIT" if not exist "%install_dir%\x64\Debug\SDL2.dll" (
     set d64=T
     echo SDL2.dll not found, attempting to build
 )
-if "%OS%"=="64BIT" if not exist %install_dir%\x64\Debug\SDL2.lib (
+if "%OS%"=="64BIT" if not exist "%install_dir%\x64\Debug\SDL2.lib" (
     set d64=T
     echo SDL2.lib not found, attempting to build
 )
-if "%OS%"=="64BIT" if not exist %install_dir%\x64\Debug\SDL2main.lib (
+if "%OS%"=="64BIT" if not exist "%install_dir%\x64\Debug\SDL2main.lib" (
     set d64=T
     echo SDL2main.lib not found, attempting to build
 )
-if "%OS%"=="64BIT" if not exist %install_dir%\x64\Debug\SDL2test.lib (
+if "%OS%"=="64BIT" if not exist "%install_dir%\x64\Debug\SDL2test.lib" (
     set d64=T
     echo SDL2test.lib not found, attempting to build
 )
@@ -144,19 +152,19 @@ if %d64%==T (
 
 echo Checking %install_dir%\x64\Release\
 set r64=F
-if "%OS%"=="64BIT" if not exist %install_dir%\x64\Release\SDL2.dll (
+if "%OS%"=="64BIT" if not exist "%install_dir%\x64\Release\SDL2.dll" (
     set r64=T
     echo SDL2.dll not found, attempting to build
 )
-if "%OS%"=="64BIT" if not exist %install_dir%\x64\Release\SDL2.lib (
+if "%OS%"=="64BIT" if not exist "%install_dir%\x64\Release\SDL2.lib" (
     set r64=T
     echo SDL2.lib not found, attempting to build
 )
-if "%OS%"=="64BIT" if not exist %install_dir%\x64\Release\SDL2main.lib (
+if "%OS%"=="64BIT" if not exist "%install_dir%\x64\Release\SDL2main.lib" (
     set r64=T
     echo SDL2main.lib not found, attempting to build
 )
-if "%OS%"=="64BIT" if not exist %install_dir%\x64\Release\SDL2test.lib (
+if "%OS%"=="64BIT" if not exist "%install_dir%\x64\Release\SDL2test.lib" (
     set r64=T
     echo SDL2test.lib not found, attempting to build
 )
